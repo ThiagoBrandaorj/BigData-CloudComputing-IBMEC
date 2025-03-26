095e185d6c56832e802e257fd163f937d15e3aa3
THIAGO CORREA BRANDAO <202301135265@alunos.ibmec.edu.br>
2025-03-26 19:09:40 -0300

    ajuste na criação dos models e controllers do endereco

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.database import db
 from app.config import Config
 from app.controllers.usuario_controller import usuario_bp
+from app.controllers.endereco_controller import endereco_bp
 
 def create_app():
     app = Flask(__name__)
@@ -10,5 +11,6 @@ def create_app():
     db.init_app(app)
 
     app.register_blueprint(usuario_bp, url_prefix="/usuario")
+    app.register_blueprint(endereco_bp, url_prefix="/endereco")
 
     return app
