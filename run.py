dbfd09fc05bc28a329dbc06e21f0c59597e93991
GuiDDuran <guilherme.d.gea@gmail.com>
2025-03-30 18:36:39 -0300

    Testando erros no app service

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from app import create_app
 from app.database import db
 
@@ -7,4 +8,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))  # Usa a porta do ambiente se disponível
+    app.run(host="0.0.0.0", port=port, debug=True)
