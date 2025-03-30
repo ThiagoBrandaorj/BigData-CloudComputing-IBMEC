0349231785470d54a4ad67dd7cad9ee7039c9f08
GuiDDuran <guilherme.d.gea@gmail.com>
2025-03-30 18:07:12 -0300

    Alterando caminho dos bancos para p azure

--- app/config.py
+++ app/config.py
@@ -1,11 +1,9 @@
 import os
 
 class Config:
-    # configuração banaco relacional
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://root:admin@localhost/ecommerce")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://grupo:administrador99*@ibmec-cloud-ecommerce-gr.mysql.database.azure.com/ecommerce?ssl_verify_cert=false")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # configuração banco Nosql
-    AZURE_COSMOS_URI = "https://localhost:8081"
-    AZURE_COSMOS_KEY = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
-    AZURE_COSMOS_DATABASE = "ibmec-cloud-mall-products"
+    AZURE_COSMOS_URI = "https://ibmec-cloud-ecommerce-35625.documents.azure.com:443/"
+    AZURE_COSMOS_KEY = "nOMr6CyGGmaFiCEVHHPXqEbRk8LuaaQW24TQIxe41OnuDJetd1wQvP2ONw4UgYAK0F9rkVCpKRcVACDb1AMpsg=="
+    AZURE_COSMOS_DATABASE = "ecommerce-produtos"
